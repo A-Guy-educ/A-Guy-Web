62891fa075c4d188c165b3d308f79ab0b07885b9
Shai <kotz96@gmail.com>
2025-12-31 16:06:31 +0200

    chore: rename honeypot.css to SignupFormFields.css
    CSS file should match component name convention.

--- src/app/(frontend)/signup/SignupFormFields.tsx
+++ src/app/(frontend)/signup/SignupFormFields.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import './honeypot.css'
+import './SignupFormFields.css'
 
 interface SignupFormFieldsProps {
   t: (key: string) => string
